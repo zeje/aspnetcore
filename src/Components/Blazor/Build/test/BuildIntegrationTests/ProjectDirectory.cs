0b38e271a0dc3b18b7e7803727188b348a37ebcf
Hao Kung <HaoK@users.noreply.github.com>
2020-03-19 11:29:45 -0700

    Update ProjectDirectory.cs

--- src/Components/Blazor/Build/test/BuildIntegrationTests/ProjectDirectory.cs
+++ src/Components/Blazor/Build/test/BuildIntegrationTests/ProjectDirectory.cs
@@ -129,7 +129,9 @@ $@"<Project>
             static void CopyRepositoryAssets(string projectRoot)
             {
                 const string GlobalJsonFileName = "global.json";
-                var globalJsonPath = Path.Combine(RepoRoot, GlobalJsonFileName);
+                var path = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("helix")) 
+                    ? RepoRoot : Directory.GetCurrentDirectory();
+                var globalJsonPath = Path.Combine(path, GlobalJsonFileName);
 
                 var destinationFile = Path.Combine(projectRoot, GlobalJsonFileName);
                 File.Copy(globalJsonPath, destinationFile);
