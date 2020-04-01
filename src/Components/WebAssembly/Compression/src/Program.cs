574d4567c4877b7f44fe9d399b89d985ffafc7b3
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-04-01 05:23:21 -0700

    Fix all tests

--- src/Components/WebAssembly/Compression/src/Program.cs
+++ src/Components/WebAssembly/Compression/src/Program.cs
@@ -21,7 +21,7 @@ namespace Microsoft.AspNetCore.Components.WebAssembly.Build.BrotliCompression
 
             using var sourceStream = File.OpenRead(args[0]);
             using var fileStream = new FileStream(args[1], FileMode.Create);
-            using var stream = new BrotliStream(fileStream, CompressionLevel.Optimal);
+            using var stream = new BrotliStream(fileStream, CompressionLevel.Fastest);
 
             await sourceStream.CopyToAsync(stream);
 
